1627fadc72f9e6813db670823b7307faac675534
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-03-20 20:08:12 -0600

    implements the element node class for the pins

---
+++ Machine/ElementPinNode.py
@@ -0,0 +1,6 @@
+from Elements.Element import Element
+class ElementNode:
+    def __init__(self,element):
+        self.element : Element = element
+        self.next = None
+        self.prev = None
